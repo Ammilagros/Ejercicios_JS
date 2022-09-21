60805991a2bbf246957c672e099d062d725ef6af
Milagros Andres <77891699+Ammilagros@users.noreply.github.com>
2022-09-21 15:02:32 -0300

    Add files via upload

---
+++ Ejercicio_Strings.js
@@ -0,0 +1,20 @@
+
+let nombre          = "Milagros";
+let apellido        = "Andres";
+let estudiante      = nombre + " " + apellido;
+
+//Strings en mayuscula y minuscula
+let estudianteMayus = estudiante.toUpperCase;
+let estudianteMinus = estudiante.toLowerCase;
+
+//Conteo de Strings
+let conteo        = estudiante.length;
+let primera_letra = estudiante.charAt(0);
+let ultima_letra  = estudiante.charAt(15);
+
+//Eliminar espacios
+let estudiante_sinEspacios = estudiante.trim;
+
+//Variable booleana
+
+console.log(estudiante.includes("Milagros"));
