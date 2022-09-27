ef78be5e66b011aaa6aea797691557add23db71b
Milagros Andres <77891699+Ammilagros@users.noreply.github.com>
2022-09-27 09:48:52 -0300

    Add files via upload

---
+++ Ejercicio_numeros.js
@@ -0,0 +1,17 @@
+//Una variable que contenga tu altura en centímetros (entero)
+let altura_centimetros = 160;
+
+// Una variable que contenga tu altura en metros (número de coma flotante)
+let altura_metros = 1.60;
+
+// Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let peso_kg = 55.0;
+
+//Una variable que contenga tu altura en metros redondeada hacia arriba
+let altura_redondeada_arriba = altura_metros.toPrecision(2);
+
+//Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let altura_redondeada_abajo = altura_metros.toFixed(2);
+
+//Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let max_valor = "el máximo valor que se puede obtener en Javascript + 1".Number.MAX_VALUE;
