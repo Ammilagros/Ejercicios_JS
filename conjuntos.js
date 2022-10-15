d42903d68a64f34082f9384e3556336a92d69cab
Milagros Andres <77891699+Ammilagros@users.noreply.github.com>
2022-10-15 15:35:26 -0300

    Add files via upload

---
+++ conjuntos.js
@@ -0,0 +1,8 @@
+// Un nuevo Set con los nombres de tu familia
+let familia = new Set(["Emilio", "Ian", "Benji"]);
+
+//Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+familia.add("Milagros");
+
+//Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+familia.add("JavaScript");
