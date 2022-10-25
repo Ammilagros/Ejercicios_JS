24129cc61014f59e2bfc44ed43d6555a44ddb588
Milagros Andres <77891699+Ammilagros@users.noreply.github.com>
2022-10-25 13:18:39 -0300

    Gestion de errores 2

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const winston = require('winston');
+const logger = require("./logger");
 
-const logger = winston.createLogger({
-    level: 'debug',
-    format: winston.format.json(),
-    //defaultMeta: { service: 'user-service' },
-    transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    // Se utiliza para loggear(registrar) errores y cosas de exito
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-    ],
-});
-
-module.exports = logger;
+//logger.log("Hola estoy salinedo por pantalla") - no contemplado por winston
+logger.info("Esto es un mensaje informativo")
+logger.debug("Esto es un mensaje de bugg")
+logger.warn("Esta es una advertencia")
 
 
+//En el archivo index.js crea una función que devuelva un error con un mensaje personalizado
+    function ejercicio_error(valor){
+        if(typeof valor === "number"){
+             return valor * 2;
+        }
+    }
+    throw new Error("El valor debe ser de tipo number");
+    const doble = ejercicio_error(valor);
 
+//Registra el error en un archivo a través de un try...catch
+
+try {
+    console.log("Esra ejecutandose de manera correcta")
+    const doble = ejercico_eror("veintitres")
+}catch(e){
+    console.log("ERROR!")
+}
